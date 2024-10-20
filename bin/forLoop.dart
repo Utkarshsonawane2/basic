a86e31def452b59a4231e3542aabf76cca7b9447
Utkarsh sonawane <utkarshsonawane3421@gmail.com>
2024-10-20 12:30:15 +0530

    the loop code

---
+++ bin/forLoop.dart
@@ -0,0 +1,8 @@
+// all loop in one programme
+
+void main() {
+  // using for loop
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
---
+++ bin/whileLoop.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 10;
+  // while Loop
+  while (a >= 1) {
+    if (a == 5) {
+      print('Hello');
+    } else {
+      print(a);
+    }
+
+    a--;
+  }
+}
